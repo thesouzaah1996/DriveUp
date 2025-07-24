b18c0d7b2c5cf0f72539727d7f5e5478f550ed74
Matheus <matheus.de.souza.code@gmail.com>
2025-07-24 10:40:57 -0300

    first commit

---
+++ src/main/java/com/driveup/driveup/DriveupApplication.java
@@ -0,0 +1,13 @@
+package com.driveup.driveup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driveup/driveup/DriveupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveup.driveup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
